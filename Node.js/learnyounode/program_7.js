6b10405f3d80949719ff6baf8d028ac45449d5b7
PawelMacan <pavelm86@gmail.com>
2018-09-16 00:15:44 +0200

    Exercise 7 - Http GET completed

---
+++ Node.js/learnyounode/program_7.js
@@ -0,0 +1,11 @@
+// console.log(process.argv[2])
+
+var http = require('http');
+
+http.get(process.argv[2], function(res) {
+	res.setEncoding('utf8');
+	res.on("data", function(data){
+		
+		console.log(data);
+	})
+});
